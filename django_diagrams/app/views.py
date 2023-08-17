d4c2751c94cd2e1219bb00e599f1bf226765a6d6
Kirill Kudryashov <b1nc0d3.kka@gmail.com>
2023-08-17 19:19:09 +0300

    feature: added logic to view

--- django_diagrams/app/views.py
+++ django_diagrams/app/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from app.models import City
 
-# Create your views here.
 
-
-def some_view(request):
-    return render(request, 'graph_chartjs.html', context={'data': {'cities': ['A', 'B', 'C']}})
+def get_cities(request):
+    cities = City.objects.all()
+    data = []
+    for city in cities:
+        planned_income = []
+        fact_income = []
+        city_plans = city.city_plan.all()
+        city_facts = city.city_fact.all()
+        print(city_plans)
+        for i in range(city_plans.count()):
+            print(city_plans[i].income)
+            planned_income.append({'year': city_plans[i].year.year, 'income': city_plans[i].income})
+            fact_income.append({'year': city_facts[i].year.year, 'income': city_facts[i].income})
+        json_city = {
+            'name': city.name,
+            'planned_income': planned_income,
+            'fact_income': fact_income}
+        data.append(json_city)
+    print(data)
+    return render(request,
+                  'graph_chartjs.html',
+                  context={'data': data})
--- django_diagrams/django_diagrams/urls.py
+++ django_diagrams/django_diagrams/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from app.views import some_view
+from app.views import get_cities
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('', some_view),
+    path('', get_cities),
     path('admin/', admin.site.urls),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
