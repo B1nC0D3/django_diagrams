e9624b7d824b2325621d848094b363f2090dfe62
Kirill Kudryashov <b1nc0d3.kka@gmail.com>
2023-08-17 15:05:43 +0300

    feature: added models and views

--- django_diagrams/app/models.py
+++ django_diagrams/app/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class City(models.Model):
+    name = models.CharField(max_length=256, verbose_name='Название города')
+
+
+class CityPlan(models.Model):
+    city = models.ForeignKey(City,
+                             on_delete=models.CASCADE,
+                             related_name='city_plan')
+    year = models.DateField(verbose_name='Год')
+    income = models.DecimalField(max_digits=16,
+                                 decimal_places=2,
+                                 null=True,
+                                 blank=True,
+                                 verbose_name='Ожидаемый доход')
+
+
+class CityFact(models.Model):
+    city = models.ForeignKey(City,
+                             on_delete=models.CASCADE,
+                             related_name='city_fact')
+    year = models.DateField(verbose_name='Год')
+    income = models.DecimalField(max_digits=16,
+                                 decimal_places=2,
+                                 null=True,
+                                 blank=True,
+                                 verbose_name='Фактический доход')
--- django_diagrams/app/views.py
+++ django_diagrams/app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def some_view(request):
+    return render(request, 'graph_chartjs.html', context={'data': {'cities': ['A', 'B', 'C']}})
