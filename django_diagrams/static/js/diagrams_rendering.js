480ce7611ced24794629323506f00bef12dd2478
Kirill Kudryashov <b1nc0d3.kka@gmail.com>
2023-08-18 11:48:12 +0300

    feature: added diagrams rendering logic

--- django_diagrams/static/js/diagrams_rendering.js
+++ django_diagrams/static/js/diagrams_rendering.js
@@ -1,3 +1,26 @@
-const data = document.currentScript.nextElementSibling.textContent
-console.log(JSON.parse(data))
+const raw_data = document.currentScript.nextElementSibling.textContent
+const data = JSON.parse(raw_data)
+const ctx = document.getElementById('CityDiagram')
+const datasets = []
+let labels = new Set()
+
+for (let city of data) {
+    const incomes = []
+    for (let income of city.planned_income) {
+        labels.add(income.year)
+        incomes.push(income.income)
+    }
+    const dataset = {
+        'label': city.name,
+        'data': incomes,
+        backgroundColor: 'rgba(220, 220, 220, 0.5)',
+        pointBorderColor: "#fff",
+    }
+    datasets.push(dataset)
+}
+
+labels = Array.from(labels)
+const diagramData = {labels: labels, datasets: datasets}
+
+new Chart(ctx, {type: 'bar', data: diagramData, options: {responsive: true}})
 
